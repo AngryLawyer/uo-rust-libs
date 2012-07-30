384aff43e77c33639435a5c599aaff878f3edf8d
Tony Aldridge <zaragopha@hotmail.com>
2012-07-30 14:04:48 +0100

    Debugging of Bitmap writing functions

--- src/art.rs
+++ src/art.rs
@@ -33,37 +33,34 @@ fn load_tiles(root_path: ~str) -> ~[map_tile] {
     ret result;
 }
 
-fn map_tile_to_bitmap(tile: map_tile) -> ~[u8] {
-    let bmp_header: ~[u8] = ~[0x42, 0x4D];
-    //SIZE
-    let file_size: ~[u8] = byte_helpers::uint_to_le_bytes(7744 + 54, 4);
-    let spacer: ~[u8] = ~[0, 0, 0, 0];
-    let offset: ~[u8] = byte_helpers::uint_to_le_bytes(52, 2);
+fn map_tile_to_bitmap(/*tile: map_tile*/) -> ~[u8] {
+    let signature: ~[u8] = ~[0x42, 0x4D];
+    let file_size: ~[u8] = byte_helpers::uint_to_le_bytes(7744 + 14 + 40, 4);
+    let reserved: ~[u8] = ~[0, 0, 0, 0];
+    let data_offset: ~[u8] = byte_helpers::uint_to_le_bytes(54, 4);
 
-    let header: ~[u8] = byte_helpers::uint_to_le_bytes(40, 4);
-    
-    let width: ~[u8] = byte_helpers::uint_to_le_bytes(40, 4); //FIXME: should be signed
-    let height: ~[u8] = byte_helpers::uint_to_le_bytes(40, 4);
-
+    let header_size: ~[u8] = byte_helpers::uint_to_le_bytes(40, 4);
+    let width: ~[u8] = byte_helpers::uint_to_le_bytes(44, 4); //FIXME: should be signed
+    let height: ~[u8] = byte_helpers::uint_to_le_bytes(44, 4);
     let colour_panes: ~[u8] = ~[1, 0];
     let depth: ~[u8] = ~[16, 0];
     let compression: ~[u8] = ~[0,0,0,0];
-
-    let image_size: ~[u8] = byte_helpers::uint_to_le_bytes(7744, 4);
+    let image_size: ~[u8] = ~[0,0,0,0];//byte_helpers::uint_to_le_bytes(7744, 4);
     let horizontal_res: ~[u8] = ~[0, 0, 0, 0];
     let vertical_res: ~[u8] = ~[0, 0, 0, 0];
     let palette_count: ~[u8] = ~[0, 0, 0, 0];
     let important_colours: ~[u8] = ~[0, 0, 0, 0];
 
-    let padding: ~[u8] = ~[0, 0]; //TODO: We are out by 2. Recheck spec.
     //54 bytes so far
     //44 pixels per row, each 3 bytes
     //44 columns
     
     //Here's where it gets crazy - image data is stored as 2, 4, 8,  up to 44, then 44 down again
     let mut pixels: ~[u8] = ~[];
-    /*for uint::range(0, 44) |i| {
-        if (i < 22) {
+    for uint::range(0, 44) |i| {
+        vec::grow(pixels, 22 * 2, 0x00);
+        vec::grow(pixels, 22 * 2, 0xFF);
+        /*if (i < 22) {
             vec::grow(pixels, (22 - (i + 1)) * 3, 0xff);
             vec::grow(pixels, ((i + 1) * 2) * 3, 0);
             vec::grow(pixels, (22 - (i + 1)) * 3, 0xff);
@@ -71,16 +68,17 @@ fn map_tile_to_bitmap(tile: map_tile) -> ~[u8] {
             vec::grow(pixels, ((i + 1) - 22) * 3, 0xff);
             vec::grow(pixels, (44 - ((i + 1) * 2)) * 3, 0);
             vec::grow(pixels, ((i + 1) - 22) * 3, 0xff);
-        }
-    }*/
-    vec::grow(pixels, 44 * 44 * 4, 0x7f);
+        }*/
+    }
+    //vec::grow(pixels, 44 * 44 * 4, 0x7f);
 
     ret vec::concat(~[
-        bmp_header,
+        signature,
         file_size,
-        spacer,
-        offset,
-        header,
+        reserved,
+        data_offset,
+
+        header_size,
         width,
         height,
         colour_panes,
@@ -91,7 +89,7 @@ fn map_tile_to_bitmap(tile: map_tile) -> ~[u8] {
         vertical_res,
         palette_count,
         important_colours,
-        padding,
+
         pixels
     ]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     let path = ~"../uo-aos/";
-    let skills: ~[skills::skill] = skills::load_skills(path);
+    /*let skills: ~[skills::skill] = skills::load_skills(path);
 
     for skills.each |skill| {
         io::println(str::from_bytes(skill.name)); 
     }
 
     let art: ~[art::map_tile] = art::load_tiles(path);
-
-    let bmp_data: ~[u8] = art::map_tile_to_bitmap(art[0]);
+*/
+    let bmp_data: ~[u8] = art::map_tile_to_bitmap();
 
     //Test writing bitmap
     let maybe_writer = io::file_writer(~"./test.bmp", ~[io::create, io::truncate]);
