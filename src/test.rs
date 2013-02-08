3599aae3a874e8ff32919e9a78f96449b9be0544
Tony Aldridge <tony@dabapps.com>
2013-02-08 13:40:11 +0000

    Changed to Die, rather than Fail

--- src/test.rs
+++ src/test.rs
@@ -6,7 +6,7 @@ mod skills {
     fn test_skills() {
         match ::skills::SkillReader(&path::Path(~"files/skills.idx"), &path::Path(~"files/skills.mul")) {
             result::Err(msg) => {
-                fail!(msg);
+                die!(msg);
             },
             result::Ok(skill_reader) => {
                 match skill_reader.read_skill(0) {
@@ -15,7 +15,7 @@ mod skills {
                         assert skill.clickable == false;
                     },
                     option::None => {
-                        fail!(~"No skill at 0");
+                        die!(~"No skill at 0");
                     }
                 };
                 //There should be a skill at #10
@@ -24,13 +24,13 @@ mod skills {
                         ()
                     },
                     option::None => {
-                        fail!(~"No skill at 10");
+                        die!(~"No skill at 10");
                     }
                 };
                 //There shouldn't be 100 skills
                 match skill_reader.read_skill(100) {
                     option::Some(_skill) => {
-                        fail!(~"Skill at 100");
+                        die!(~"Skill at 100");
                     },
                     option::None => ()
                 };
@@ -42,7 +42,7 @@ mod skills {
     fn test_read_all() {
         match ::skills::load_skills(&path::Path(~"files/skills.idx"), &path::Path(~"files/skills.mul")) {
             result::Err(msg) => {
-                fail!(msg);
+                die!(msg);
             },
             result::Ok(skill_list) => {
                 assert skill_list.len() >= 49 //49 skills in the original UO
@@ -60,22 +60,9 @@ mod art {
     fn test_read_tile() {
         match ::art::TileReader(&path::Path(~"files/artidx.mul"), &path::Path(~"files/art.mul")) {
             result::Err(msg) => {
-                fail!(msg);
+                die!(msg);
             },
             result::Ok(tileReader) => {
-                /*match tileReader.read_tile(400) {
-                    option::Some(tile) => {
-                        assert tile.raw_image.len() == 1012; 
-                        let bitmap = tile.with_transparency(0xF000);
-                        assert bitmap.len() == (44*44);
-                        assert bitmap[0] == 0xF000;
-                        assert bitmap[21] != 0xF000;
-                    },
-                    option::None => {
-                        error!("Couldn't read tile 400")
-                        fail;
-                    }
-                };*/
                 match tileReader.read_tile(100) {
                     option::Some(tile) => {
                         assert tile.raw_image.len() == 1012; 
@@ -85,12 +72,12 @@ mod art {
                         assert bitmap[21] != 0xF000;
                     },
                     option::None => {
-                        fail!(~"Couldn't read tile 100")
+                        die!(~"Couldn't read tile 100")
                     }
                 };
                 match tileReader.read_tile(0x4000) {
                     option::Some(_tile) => {
-                        fail!(~"Read static 0x4000 as tile")
+                        die!(~"Read static 0x4000 as tile")
                     },
                     option::None => ()
                 };
@@ -102,7 +89,7 @@ mod art {
     fn test_read_static() {
         match ::art::TileReader(&path::Path(~"files/artidx.mul"), &path::Path(~"files/art.mul")) {
             result::Err(msg) => {
-                fail!(msg);
+                die!(msg);
             },
             result::Ok(tile_reader) => {
                 match tile_reader.read_static(0x4000) {
@@ -112,7 +99,7 @@ mod art {
                         assert bitmap.len() == (static_tile.width + static_tile.height) as uint;
                     },
                     option::None => {
-                        fail!(~"Couldn't read tile 0x4000");
+                        die!(~"Couldn't read tile 0x4000");
                     }
                 };
             }
