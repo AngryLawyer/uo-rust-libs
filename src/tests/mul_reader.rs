f46cb7604d36604f6b332d336ce609dc9114c951
Tony Aldridge <zaragopha@hotmail.com>
2014-06-07 14:30:01 +0100

    More broken tests

--- src/tests/mul_reader.rs
+++ src/tests/mul_reader.rs
@@ -3,7 +3,7 @@ use mul_reader::MulReader;
 #[test]
 fn test_load_mulreader() {
     match MulReader::new(&Path::new("./src/tests/testdata/test_skills.idx"), &Path::new("./src/tests/testdata/test_skills.mul")) {
-        Ok(mul_reader) => {
+        Ok(_mul_reader) => {
             //Passed
         },
         Err(message) => fail!(message)
@@ -16,11 +16,29 @@ fn test_read_first_entry() {
         Ok(mut mul_reader) => {
             let record = mul_reader.read(0);
             match record {
-                Some(record) => {
+                Some(_record) => {
                     //Passed
                 },
                 None => {
                     fail!("Couldn't find a record!")
+                }
+            }
+        },
+        Err(message) => fail!(message)
+    }
+}
+
+#[test]
+fn test_read_impossible_entry() {
+    match MulReader::new(&Path::new("./src/tests/testdata/test_skills.idx"), &Path::new("./src/tests/testdata/test_skills.mul")) {
+        Ok(mut mul_reader) => {
+            let record = mul_reader.read(999999);
+            match record {
+                Some(_record) => {
+                    fail!("Unexpectedly read a result from a known impossible address")
+                },
+                None => {
+                    //Passed
                 }
             }
         },
--- src/tests/skills.rs
+++ src/tests/skills.rs
@@ -3,7 +3,8 @@ use skills::Skills;
 #[test]
 fn test_load_skills() {
     match Skills::new(&Path::new("./src/tests/testdata/test_skills.idx"), &Path::new("./src/tests/testdata/test_skills.mul")) {
-        Ok(skills) => {
+        Ok(_skills) => {
+            //Passed
         },
         Err(message) => fail!(message)
     }
