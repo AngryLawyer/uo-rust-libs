be70cbfff4fa03d8dc1e01d775f6accbb7a192db
Tony Aldridge <tony@dabapps.com>
2014-12-09 13:53:53 +0000

    Continuing working on art tests

--- src/tests/test_art.rs
+++ src/tests/test_art.rs
@@ -14,8 +14,17 @@ fn test_load_tile() {
     };
 }
 
-#[test]
+/*#[test]
 fn dump_art() {
     use mul_reader::MulWriter;
-    let mul_writer = MulWriter::new(&Path::new("./testdata/test_art.idx"), &Path::new("./testdata/test_art.mul")).ok().expect("Can't open files for writing");
-}
+    use art::{Art, Tile};
+    use std::io::FileMode;
+    let dummy_tile = Tile {header: 0, image_data: [0xFFFF, ..1022]};
+    let mut mul_writer = MulWriter::new(&Path::new("./testdata/test_art.idx"), &Path::new("./testdata/test_art.mul"), FileMode::Truncate).ok().expect("Can't open files for writing");
+
+    let serialized = dummy_tile.serialize();
+
+    for i in range(0, 0x4000u32) {
+        mul_writer.append(&serialized, None, None).ok().expect("Can't write white tile");
+    }
+}*/
