5ce1c544de3ee4a351efc52214cdfcff885a0352
Tony Aldridge <zaragopha@hotmail.com>
2016-12-07 20:53:49 +0000

    Radar colour reader

--- src/map.rs
+++ src/map.rs
@@ -3,6 +3,7 @@ use std::fs::{File};
 use std::path::Path;
 use byteorder::{LittleEndian, ReadBytesExt};
 use mul_reader::MulReader;
+use color::Color16;
 
 pub const BLOCK_SIZE: usize = 196;
 pub const OFFSET: u32 = 4;
@@ -138,5 +139,35 @@ impl StaticReader {
                 format!("{} {} is outside of valid map coordinates", x, y)
             ))
         }
+    }
+}
+
+pub struct RadarColReader {
+    data_reader: File
+}
+
+impl RadarColReader {
+    pub fn new(radar_col_path: &Path) -> Result<RadarColReader> {
+        let data_reader = try!(File::open(radar_col_path));
+
+        Ok(RadarColReader {
+            data_reader: data_reader,
+        })
+    }
+
+    pub fn read_color(&mut self, id: u32) -> Result<Color16> {
+        try!(self.data_reader.seek(SeekFrom::Start((id * 2) as u64)));
+        let data = try!(self.data_reader.read_u16::<LittleEndian>());
+        Ok(data)
+    }
+
+    pub fn read_colors(&mut self) -> Result<Vec<Color16>> {
+        try!(self.data_reader.seek(SeekFrom::Start(0)));
+        let meta = try!(self.data_reader.metadata());
+        let mut output = vec![];
+        for _i in 0..(meta.len() / 2) {
+            output.push(try!(self.data_reader.read_u16::<LittleEndian>()));
+        }
+        Ok(output)
     }
 }
